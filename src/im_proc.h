79d852042ad4de536157cbcca2b47fb325150ea2
James Mare <jamesmare12@gmail.com>
2016-01-20 09:16:46 +1100

    commenting and cleaning up

--- src/im_proc.h
+++ src/im_proc.h
@@ -18,25 +18,28 @@ class im_proc
            
         void init_feed(int ID);
         void process_frame(); 
+
         cv::Mat get_frame_overlay();
         cv::Mat get_frame_thresholded(int feedID);
         
 
     private:
+        //FUNCTIONS
         void threshold_frame(cv::Mat *frame, std::vector<int> *params);
         void morph_frame(cv::Mat *frame, std::vector<int> *params);
         void loadframe(cv::Mat *frame);
         void writeframe(cv::Mat *frame);
-
+        void overlay_position(cv::Mat *frame);
 
         std::tuple<bool, double, double> trackObject(cv::Mat *frame); 
         std::tuple<bool, double, double> filterpositions(std::tuple<bool, double, double> pos1, std::tuple<bool, double, double> pos2); 
+        
+        //VARIABLES AND OBJECTS
         cv::Mat mainfeed;
         cv::Mat frame_proc1;
         cv::Mat frame_proc2;
-        cv::Mat feed_conbri;
+        
         cv::VideoCapture cap;
-        void overlay_position(cv::Mat *frame);
 
         std::tuple<bool, double, double> Pos1;
         std::tuple<bool, double, double> Pos2;
