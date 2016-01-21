c5ae63d8a91ccaa8631d711dcfa4ea883f6e9feb
James Mare <jamesmare12@gmail.com>
2016-01-21 13:51:11 +1100

    removed hough for now

--- src/im_proc.cpp
+++ src/im_proc.cpp
@@ -135,30 +135,12 @@ void im_proc::threshold_frame(Mat *frame, vector<int> *params)
 void im_proc::adaptivethreshold_frame(Mat *frame)
 {
     cvtColor(*frame, *frame, CV_RGB2GRAY);
-    //threshold(*frame, *frame, 100, 255, THRESH_BINARY);
+    threshold(*frame, *frame, 180, 255, THRESH_BINARY);
     adaptiveThreshold( *frame, *frame, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 11, 10);
 
     morph_frame(frame, &imParams1);
-    Mat src_gray = frame->clone();
     
 
-    GaussianBlur( src_gray, src_gray, Size(5, 5), 1, 1 );
-
-  vector<Vec3f> circles;
-
-  /// Apply the Hough Transform to find the circles
-  HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows/8, 200, 50, 1, 0 );
-
-  /// Draw the circles detected
-  for( size_t i = 0; i < circles.size(); i++ )
-  {
-      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-      int radius = cvRound(circles[i][2]);
-      // circle center
-      circle( mainfeed, center, 3, Scalar(0,255,0), -1, 8, 0 );
-      // circle outline
-      circle( mainfeed, center, radius, Scalar(0,0,255), 3, 8, 0 );
-   }
 }
 
 
