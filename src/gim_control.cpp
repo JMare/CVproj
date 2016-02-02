4218db2ffb47b75bbab8da443ac5bb925e00b7ba
James Mare <jamesmare12@gmail.com>
2016-02-02 11:20:28 +1100

    trivedit

--- src/gim_control.cpp
+++ src/gim_control.cpp
@@ -12,17 +12,14 @@ gim_control::gim_control()
     serial_port oPort;
 
     oPort.writeByte(41);
-    oPort.writeByte(40);
 
     uint16_t bytesavail = oPort.getBytesAvailable();
 
     cout << bytesavail << " bytes available" << endl;
 
     uint8_t read = oPort.readByte();
-    uint8_t read2 = oPort.readByte();
     
 
     cout << read << endl;
-    cout << read2 << endl;
 
 }
