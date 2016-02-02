6ef54ec7b34afe6d6c61e183f76a75efd0056bf9
James Mare <jamesmare12@gmail.com>
2016-02-02 14:04:27 +1100

    gimbal being commanded pitch and yaw

--- src/gim_control.cpp
+++ src/gim_control.cpp
@@ -7,30 +7,40 @@ using namespace std;
 
 gim_control::gim_control()
 {
-    cout << "Starting serial" << endl;
-
     serial_port oPort;
     SBGC_Parser oSbgc_parser;
     SerialCommand b;
 
     oSbgc_parser.init(&oPort);
 
-    SBGC_cmd_control_t c;
+    SBGC_cmd_control_t c = { 0, 0, 0, 0, 0, 0, 0 };
 
-    /////////////////// Demo 1. PITCH and YAW gimbal by 40 and 30 degrees both sides and return back.
-    // Actual speed depends on PID setting.
-    // Whait 5 sec to finish
+    //move gimbal to initial position
+    c.mode = SBGC_CONTROL_MODE_ANGLE;
+    c.speedROLL = c.speedPITCH = c.speedYAW = 30 * SBGC_SPEED_SCALE;
+    SBGC_cmd_control_send(c, oSbgc_parser);
+    usleep(5000000);
+
     c.mode = SBGC_CONTROL_MODE_ANGLE;
     c.anglePITCH = SBGC_DEGREE_TO_ANGLE(40);
     c.angleYAW = SBGC_DEGREE_TO_ANGLE(30);
 
     SBGC_cmd_control_send(c, oSbgc_parser);
+    
+    cout << "sent command 1" << endl;
 
+    usleep(5000000);
+
+    c.mode = SBGC_CONTROL_MODE_ANGLE;
     c.anglePITCH = SBGC_DEGREE_TO_ANGLE(-40);
     c.angleYAW = SBGC_DEGREE_TO_ANGLE(-30);
-    SBGC_cmd_control_send(c, oSbgc_parser);
 
+    SBGC_cmd_control_send(c, oSbgc_parser);
+    cout << "sent command 2" << endl;
 
     cout << "ending serial" << endl;
-
+    c.anglePITCH = 0;
+    c.angleYAW = 0;
+    SBGC_cmd_control_send(c, oSbgc_parser);
+    usleep(5000000);
 }
