e3125b50bc8b321a9a71b8273367c3eddd43bbcc
James Mare <jamesmare12@gmail.com>
2016-03-04 15:53:08 +1100

    arduino side serial api written

--- arduino/iocontrol/iocontrol.ino
+++ arduino/iocontrol/iocontrol.ino
@@ -4,8 +4,8 @@ volatile int pwm_value_2 = 0;
 volatile int prev_time_2 = 0;
 
 const byte PWM_PIN_3 = 4;
-const byte YAW_PIN = 11;
-const byte PITCH_PIN = 10;
+const byte YAW_PIN = 7;
+const byte PITCH_PIN = 8;
  
 volatile int pwm_value_3;
 
@@ -14,13 +14,18 @@ void rising_2();
 void falling_1();
 void falling_2();
 
+void piSerialWrite();
+
 #include <Servo.h>
 
 Servo Yaw;
 Servo Pitch;
 
+    int YawOut, PitchOut;
+
 void setup() {
     Serial.begin(115200);
+
     attachInterrupt(0, rising_1, RISING);
     attachInterrupt(1, rising_2, RISING);
     Serial.println("starting up");
@@ -33,19 +38,20 @@ void setup() {
 void loop() {
     pwm_value_3 = pulseIn(PWM_PIN_3, HIGH);
 
-    int YawOut, PitchOut;
 
-    if(pwm_value_3 > 1500){
-        YawOut = map(pwm_value_2,1000,2000,0,180);
-        PitchOut = map(pwm_value_1,1000,2000,0,180);
-    } else {
-        YawOut = map(1500,1000,2000,0,180);
-        PitchOut = map(1500,1000,2000,0,180);
-    }
+//    if(pwm_value_3 > 1500){
+//        YawOut = map(pwm_value_2,1000,2000,0,180);
+//        PitchOut = map(pwm_value_1,1000,2000,0,180);
+//    } else {
+//        YawOut = map(1500,1000,2000,0,180);
+//        PitchOut = map(1500,1000,2000,0,180);
+//    }
+    delay(300);
+    piSerialRead();
 
     Yaw.write(YawOut);
     Pitch.write(PitchOut);
-
+/*
     Serial.print("Pitch: ");
     Serial.print(pwm_value_1);
     Serial.print(" Yaw: ");
@@ -57,8 +63,63 @@ void loop() {
     Serial.print(" YawOut: ");
     Serial.print(YawOut);
     Serial.print("\n\r");
+    */
+
+    piSerialWrite();
 
   }
+
+void piSerialWrite()
+{
+    Serial.write("<");
+    Serial.write("1:");
+    Serial.print(pwm_value_1);
+    Serial.println(">");
+    Serial.write("<");
+    Serial.write("2:");
+    Serial.print(pwm_value_2);
+    Serial.println(">");
+    Serial.write("<");
+    Serial.write("3:");
+    Serial.print(pwm_value_3);
+    Serial.println(">");
+}
+
+void piSerialRead()
+{
+    char buffer[10];
+    char c;
+    bool endfound = false;
+
+    while(Serial.available() > 0 && !endfound) {
+        c = Serial.read();
+        if( c == '<'){
+            for(int n = 0; n < 7; n++){
+                c = Serial.read();
+                buffer[n] = c;
+            }
+            endfound = true;
+        }
+    }
+
+    if(buffer[6] == '>'){ //then we got a complete command
+        if(buffer[0] == '1'){
+            char numberext[5] = {buffer[2], buffer[3], buffer[4], buffer[5], '\0'};
+            int i;
+            PitchOut = map(atoi(numberext),1000,2000,0,180);
+            Serial.print("pitch is");
+            Serial.println(PitchOut);
+        }
+        else if(buffer[0] == '2'){
+            char numberext[5] = {buffer[2], buffer[3], buffer[4], buffer[5], '\0'};
+            int i;
+            YawOut = map(atoi(numberext),1000,2000,0,180);
+            Serial.print("yaw is");
+            Serial.println(YawOut);
+        }
+    }
+}
+        
  
 void rising_1() {
     attachInterrupt(0, falling_1, FALLING);
