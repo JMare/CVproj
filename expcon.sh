27a8cf48600dc3dbd78fc0e429e4f18c6c09f99a
james <jamesmare12@gmail.com>
2016-04-28 09:05:11 +1000

    moved scripts into test out of main dir

--- expcon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-v4l2-ctl -d /dev/video1 -c exposure_auto=1 -c exposure_auto_priority=0 -c exposure_absolute=100
-
-
-
--- startgpio.py
+++
@@ -1,8 +0,0 @@
-import RPi.GPIO as GPIO
-import time
-import sys 
-
-inPin = 21
-GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-
-GPIO.setup(inPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
